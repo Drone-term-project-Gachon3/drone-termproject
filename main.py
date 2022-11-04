59a2d0805185caa2cf855600120025c202371be0
asasia1935 <sungmin1935@naver.com>
2022-11-05 02:06:23 +0900

    final (if) drone project

---
+++ main.py
@@ -0,0 +1,80 @@
+from utils import *
+import cv2
+from recognition import *
+import time
+
+# drone init
+#myDrone = initTello()
+print("드론 초기화")
+
+# drone landing
+#myDrone.takeoff()
+print("드론 이륙")
+
+# time measure
+count = 0
+now1 = time
+mm1 = now1.localtime().tm_min
+ss1 = now1.localtime().tm_sec
+stop = False
+
+# drone move remember
+move = []
+
+while True:
+    if stop == True:
+        break
+    print("인식중")
+    condition1 = handRecognition()
+    # hand recognition
+    if condition1 == 1:
+        print("성공")
+        while True:
+            #myDrone.move_forward(5)
+            print("드론 전진 (5센티)")
+            move.append("f");
+            print(move)
+            print("인식중")
+            condition2 = handRecognition()
+            if condition2 == 0:
+                print("인식 실패")
+                #myDrone.rotate_clockwise(10)
+                print("드론 회전 (10도)")
+                move.append("r");
+                print(move)
+                break
+            elif condition2 == 2:
+                print("정지")
+                stop = True
+                break
+    elif condition1 == 0:
+        # myDrone.rotate_clockwise(10)
+        print("드론 회전 (10도)")
+        move.append("r");
+        print(move)
+    elif condition1 == 2:
+        print("정지2")
+        break
+
+# counting time measure
+now2 = time
+mm2 = now1.localtime().tm_min
+ss2 = now1.localtime().tm_sec
+
+count = (mm2 - mm1) * 60 + (ss2 - ss1)
+
+# score check
+print("드론이 쫒아간 시간은 %d초 입니다." %count)
+
+# drone return
+while move != []:
+    pop = move.pop()
+    if pop == "f":
+        #myDrone.move_forward(5)
+        print("드론 후진 (5센티)")
+    elif pop == "r":
+        #myDrone.rotate_clockwise(10)
+        print("드론 역회전 (10도)")
+
+# game over
+print("드론 원위치")
---
+++ recognition.py
@@ -0,0 +1,124 @@
+import cv2
+import mediapipe as mp
+from PIL import ImageFont, ImageDraw, Image
+import numpy as np
+
+def handRecognition():
+    # hand recognition
+    mp_drawing = mp.solutions.drawing_utils
+    mp_hands = mp.solutions.hands
+    mp_drawing_styles = mp.solutions.drawing_styles
+
+    cap = cv2.VideoCapture(0)
+
+    with mp_hands.Hands(
+            min_detection_confidence=0.5,
+            min_tracking_confidence=0.5
+    ) as hands:
+        while cap.isOpened():
+            success, image = cap.read()
+
+            if not success:
+                print("Ignoring empty camera frame.")
+
+                continue
+
+            image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+
+            image.flags.writeable = False
+            results = hands.process(image)
+
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            image_height, image_width, _ = image.shape
+
+            isRecog = False
+
+            if results.multi_hand_landmarks:
+                for hand_landmarks in results.multi_hand_landmarks:
+                    thumb_finger_state = 0
+                    if hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_CMC].y * image_height > \
+                            hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_MCP].y * image_height:
+                        if hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_MCP].y * image_height > \
+                                hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_IP].y * image_height:
+                            if hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_IP].y * image_height > \
+                                    hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP].y * image_height:
+                                thumb_finger_state = 1
+
+                    index_finger_state = 0
+                    if hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_MCP].y * image_height > \
+                            hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_PIP].y * image_height:
+                        if hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_PIP].y * image_height > \
+                                hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_DIP].y * image_height:
+                            if hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_DIP].y * image_height > \
+                                    hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * image_height:
+                                index_finger_state = 1
+
+                    middle_finger_state = 0
+                    if hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_MCP].y * image_height > \
+                            hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_PIP].y * image_height:
+                        if hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_PIP].y * image_height > \
+                                hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_DIP].y * image_height:
+                            if hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_DIP].y * image_height > \
+                                    hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP].y * image_height:
+                                middle_finger_state = 1
+
+                    ring_finger_state = 0
+                    if hand_landmarks.landmark[mp_hands.HandLandmark.RING_FINGER_MCP].y * image_height > \
+                            hand_landmarks.landmark[mp_hands.HandLandmark.RING_FINGER_PIP].y * image_height:
+                        if hand_landmarks.landmark[mp_hands.HandLandmark.RING_FINGER_PIP].y * image_height > \
+                                hand_landmarks.landmark[mp_hands.HandLandmark.RING_FINGER_DIP].y * image_height:
+                            if hand_landmarks.landmark[mp_hands.HandLandmark.RING_FINGER_DIP].y * image_height > \
+                                    hand_landmarks.landmark[mp_hands.HandLandmark.RING_FINGER_TIP].y * image_height:
+                                ring_finger_state = 1
+
+                    pinky_finger_state = 0
+                    if hand_landmarks.landmark[mp_hands.HandLandmark.PINKY_MCP].y * image_height > \
+                            hand_landmarks.landmark[mp_hands.HandLandmark.PINKY_PIP].y * image_height:
+                        if hand_landmarks.landmark[mp_hands.HandLandmark.PINKY_PIP].y * image_height > \
+                                hand_landmarks.landmark[mp_hands.HandLandmark.PINKY_DIP].y * image_height:
+                            if hand_landmarks.landmark[mp_hands.HandLandmark.PINKY_DIP].y * image_height > \
+                                    hand_landmarks.landmark[mp_hands.HandLandmark.PINKY_TIP].y * image_height:
+                                pinky_finger_state = 1
+
+                    font = ImageFont.truetype("fonts/gulim.ttc", 80)
+                    image = Image.fromarray(image)
+                    draw = ImageDraw.Draw(image)
+
+
+
+                    text = ""
+                    if thumb_finger_state == 1 and index_finger_state == 1 and middle_finger_state == 1 \
+                            and ring_finger_state == 1 and pinky_finger_state == 1:
+                        return 1
+                    # elif thumb_finger_state == 1 and index_finger_state == 1 and middle_finger_state == 0 \
+                    #     and ring_finger_state == 0 and pinky_finger_state == 0:
+                    #     text = "가위"
+                    #     print("가위")
+                    elif thumb_finger_state == 0 and index_finger_state == 0 and middle_finger_state == 0 \
+                         and ring_finger_state == 0 and pinky_finger_state == 0:
+                         return 2
+                    elif isRecog == False:
+                        return 0
+
+                    w, h = font.getsize(text)
+
+                    x = 50
+                    y = 50
+
+                    draw.rectangle((x, y, x + w, y + h), fill='black')
+                    draw.text((x, y), text, font=font, fill=(255, 255, 255))
+                    image = np.array(image)
+
+                    mp_drawing.draw_landmarks(
+                        image,
+                        hand_landmarks,
+                        mp_hands.HAND_CONNECTIONS,
+                        mp_drawing_styles.get_default_hand_landmarks_style(),
+                        mp_drawing_styles.get_default_hand_connections_style()
+                    )
+
+                if cv2.waitKey(5) & 0xFF == 27:
+                    break
+
+    cap.release()
